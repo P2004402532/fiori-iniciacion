9d83e576c0482b382ceead3f29400a2ad7e07a92
P2004402532 <johny.valencia@gco.com.co>
2024-02-15 22:11:30 +0000

    Primer commit del proyecto invoies

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ns/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ns/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
