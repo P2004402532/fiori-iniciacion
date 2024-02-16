6170207d1482eca3a568d892dd5d3ef6092f991c
P2004402532 <johny.valencia@gco.com.co>
2024-02-16 17:05:13 +0000

    3er Ejm json con selectionScreen

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -9,6 +9,10 @@ sap.ui.define([
 
         return Controller.extend("ns.invoices.controller.MainView", {
             onInit: function () {
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("./model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen"); //Si es odata no necsita este param: , "selectionScreen")
 
             }
         });
